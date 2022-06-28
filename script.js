a19cabc8d722e57d262de13c0233734c78ed7a7b
Gabriel Gonzales <106750788+GabrielGonzalesKZ@users.noreply.github.com>
2022-06-28 01:05:18 -0300

    correcao-return

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function ThroughTwenty(){
     let cont = 1
     while (cont < 21){
     console.log(cont)
-    cont++
+    cont++ 
     }
 }
 
-console.log(ThroughTwenty())
+ThroughTwenty()
 
 // Retornar os números pares de 1 a 20. (2, 4, 6,…, 18, 20)
 
@@ -18,8 +18,10 @@ function evensToTwenty(){
     for(i=1;i<21;i++)
     if(i%2==0)
     console.log(i)
+    return 
 }
-console.log(evensToTwenty())
+
+evensToTwenty()
 
 // Retornar os números ímpares de 1 a 20. (1, 3, 5,…, 17, 19)
 
@@ -28,9 +30,10 @@ function oddsToTwenty (){
     for(i=1;i<21;i++)
     if(i%2==1)
     console.log(i)
+    return
 }
 
-console.log(oddsToTwenty())
+oddsToTwenty()
 
 // Retornar os múltiplos de 5 até 100. (5, 10, 15,…, 95, 100)
 
@@ -39,22 +42,22 @@ function multiplesOfFive(){
     for(i=1;i<101;i++)
     if(i%5==0)
     console.log(i)
+    return
 }
-
-console.log(multiplesOfFive())
+multiplesOfFive()
 
 
 // Retornar os números contando de trás para frente de 20 até 1. (20, 19, 18, …, 2, 1)
 
 function countingBackwards(){
-    let cont = 20
-    while (cont > 0){
-    console.log(cont)
+    let cont = 21
+    while (cont > 1){
     cont--
+    console.log(cont)
     }
 }
 
-console.log(countingBackwards())
+countingBackwards()
 
 // Retornar todos os números até 100 que forem quadrados perfeitos. (1, 4, 9, …, 81, 100)
 
@@ -66,8 +69,9 @@ function evenNumbersBackwards(){
     for(i=20;i>0;i--)
     if(i%2==0)
     console.log(i)
+    return
 }
-console.log(evenNumbersBackwards())
+evenNumbersBackwards()
 
 
 // Retornar os números ímpares de 20 até 1. (19, 17, 15, …, 3, 1)
@@ -77,8 +81,9 @@ function oddNumbersBackwards(){
     for(i=20;i>0;i--)
     if(i%2==1)
     console.log(i)
+    return
 }
-console.log(oddNumbersBackwards())
+oddNumbersBackwards()
 
 // Retornar os múltiplos de 5 contando de trás para frente a partir de 100. (100, 95, 90, …, 10, 5)
 
@@ -86,9 +91,10 @@ console.log(oddNumbersBackwards())
         for(i=100;i>0;i--)
         if(i%5==0)
         console.log(i)
+        return
     }
-console.log(multiplesOfFiveBackwards())
 
+multiplesOfFiveBackwards()
 // Retornar os quadrados perfeitos contando de trás para frente a partir de 100. (100, 81, 64, …, 4, 1)
 
 
